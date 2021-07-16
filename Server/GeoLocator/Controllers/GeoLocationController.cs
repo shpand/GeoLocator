df39beada8750c39383f3bb8af55555d5a8bd891
aliaksandrsh <aliaksandrsh@playtika.com>
2021-07-16 10:09:15 +0300

    initial asp net core server project

---
+++ Server/GeoLocator/Controllers/GeoLocationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GeoLocator.Controllers
+{
+    [ApiController]
+    //[Route("[controller]")]
+    public class GeoLocationController : ControllerBase
+    {
+        private readonly ILogger<GeoLocationController> _logger;
+
+        public GeoLocationController(ILogger<GeoLocationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("ip/location")]
+        public Location Get(string ip)
+        {
+            return new Location
+            {
+                Country = "Belarus",
+                Region = "Minsk"
+            };
+        }
+    }
+}
---
+++ Server/GeoLocator/Location.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GeoLocator
+{
+    public class Location
+    {
+        public string Country { get; set; }
+        public string Region { get; set; }
+    }
+}
