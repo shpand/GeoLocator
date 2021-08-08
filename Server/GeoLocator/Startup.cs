336f35818b614b82574f89447505956ff677b4f9
aliaksandrsh <aliaksandrsh@playtika.com>
2021-08-08 17:42:34 +0300

    remove app.UseHttpsRedirection to be able to make requests without tls certificate

--- Server/GeoLocator/Startup.cs
+++ Server/GeoLocator/Startup.cs
@@ -38,7 +38,7 @@ namespace GeoLocator
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeoLocator v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
